1f35e7ded6e3a2f51b04be311459a217138c5da9
devromarc <rmrcslvdr@gmail.com>
2024-01-07 15:17:37 +0800

    first commit
    done with the images.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createGallery = (el) => {
+  return el
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+};
+
+const galleryImageSet = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend",galleryImageSet);
