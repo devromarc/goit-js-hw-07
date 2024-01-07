a4aee73b3ddf6baca2fdb88701dcfc662d400fe4
devromarc <rmrcslvdr@gmail.com>
2024-01-07 20:51:27 +0800

    done with the task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createGallery = (el) => {
+  return el
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>`;
+    })
+    .join("");
+};
+
+const galleryImageSet = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryImageSet);
+
+// By creating a new instance with new SimpleLightbox(...), the constructor of the SimpleLightbox class is automatically called.
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
