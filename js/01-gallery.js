ebfa0e43d0dcf0ea33b31c07da6cdb18b8aea8eb
devromarc <rmrcslvdr@gmail.com>
2024-01-07 16:22:01 +0800

    task 1 done
    adding the event listener and the basic lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -14,11 +14,39 @@ const createGallery = (el) => {
             alt="${description}"
           />
         </a>
-      </li>`;
+        </li>`;
     })
     .join("");
 };
 
 const galleryImageSet = createGallery(galleryItems);
 
-galleryContainer.insertAdjacentHTML("beforeend",galleryImageSet);
+galleryContainer.insertAdjacentHTML("beforeend", galleryImageSet);
+
+// Handle click event in the gallery
+const onGalleryClick = (event) => {
+  event.preventDefault();
+
+  // console.log(event);
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const originalURL = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`<img src="${originalURL}">`);
+
+  instance.show();
+
+  const handleOnESCpress = (event) => {
+    // console.log(event);
+    if (event.key === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", handleOnESCpress);
+    }
+  };
+  window.addEventListener("keydown", handleOnESCpress);
+};
+
+galleryContainer.addEventListener("click", onGalleryClick);
